31259d0ea126f3210003cf27930d8acc87364b69
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-02-16 19:52:48 +0700

    Create Ship factory function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+const Ships = (length) => {
+  let _hits = 0;
+  let sink = false;
+  let hit = () => _hits++;
+  let isSunk = () => {
+    if (_hits === length) {
+      sink = true
+    }
+    console.log(sink)
+  }
+
+  return { length, sink, hit, isSunk }
+};
+
+console.log("hello")
