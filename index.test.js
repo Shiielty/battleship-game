9199b1a2acf81f1c77e35a30d8cd6ef4f38b5679
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-02-16 19:53:17 +0700

    setup jest and babel for testing

---
+++ index.test.js
@@ -0,0 +1,9 @@
+// import {ships} from './index.js'
+
+// test('ships object tests', () => {
+//   expect(ships).toHaveProperty('length')
+//   expect(ships).toHaveProperty('timesHit')
+//   expect(ships).toHaveProperty('sink')
+//   expect(ships).toHaveProperty('isSunk')
+//   expect(ships).toHaveProperty('hit')
+// })
