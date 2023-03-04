90597a1654dde714dd7afcd9c4b7a2679e656623
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-03-04 21:39:08 +0700

    ref(ships.js): refactor the code

--- src/ships.js
+++ src/ships.js
@@ -1,18 +1,30 @@
-const Ships = (length) => {
-  return { 
-    length,
-    hits: 0, 
-    sink: false, 
-    hit: function() {
-      this.hits += 1
-      this.isSunk()
-    },
-    isSunk: function() {
-      if (this.length === this.hits) {
-        this.sink = true;
-      }
-    }
+const Ships = (coordinates) => {
+  let length = coordinates.length
+  let hits = 0;
+  let sink = false;
+
+  const getCoor = () => coordinates;
+  const getLength = () => length;
+  const getHits = () => hits;
+  const getSink = () => sink;
+  const hit = () => {
+    hits += 1;
   }
-};
+  const isSunk = () => {
+    if (getLength() === getHits()) {
+      sink = true;
+    }
+    return getSink();
+  }
+
+  return { 
+    getCoor,
+    getLength,
+    getHits,
+    getSink,
+    hit,
+    isSunk
+  }
+}
 
 export { Ships }
