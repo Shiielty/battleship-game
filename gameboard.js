08b36cf52909f8b89c40d75011a52b897c5e6bf4
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-03-05 10:56:29 +0700

    ref(gameboard test): create board that put each ship in specific coordinate in the board

--- gameboard.js
+++ gameboard.js
@@ -1,34 +1,31 @@
 const Gameboard = () => {
-  // const board = [[0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],
-  //                [0,1],[1,1],[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],
-  //                [0,2],[1,2],[2,2],[3,2],[4,2],[5,2],[6,2],[7,2],
-  //                [0,3],[1,3],[2,3],[3,3],[4,3],[5,3],[6,3],[7,3],
-  //                [0,4],[1,4],[2,4],[3,4],[4,4],[5,4],[6,4],[7,4],
-  //                [0,5],[1,5],[2,5],[3,5],[4,5],[5,5],[6,5],[7,5],
-  //                [0,6],[1,6],[2,6],[3,6],[4,6],[5,6],[6,6],[7,6],
-  //                [0,7],[1,7],[2,7],[3,7],[4,7],[5,7],[6,7],[7,7]]
+  const board = new Array(8).fill(null).map(() => Array(8).fill(null));
   const deployed = []
-  const record = []
+  const missedShots = []
 
+  const getBoard = () => board
   const getDeployed = () => deployed
-  const getRecord = () => record
+  const getMissedShots = () => missedShots
 
   const placeShip = (ship) => {
+    let shipCoordinate = ship.getCoor();
+    shipCoordinate.forEach(coordinate => {
+      board[coordinate[0]][coordinate[1]] = ship
+    })
     deployed.push(ship)
   }
 
   const whoIsIn = (coordinate) => {
-    return deployed.find(ship => {
-      return ship.getCoor().some(coor => coor[0] === coordinate[0] && coor[1] === coordinate[1])
-    })
+    return board[coordinate[0]][coordinate[1]]
   }
 
   const receiveAttack = (coordinate) => {
     const inCoordinate = whoIsIn(coordinate);
-    if (inCoordinate !== undefined) {
+    if (inCoordinate !== null) {
       inCoordinate.hit()
+    } else {
+      missedShots.push(coordinate)
     }
-    record.push(coordinate)
   }
 
   const isAllSunk = () => {
@@ -36,11 +33,12 @@ const Gameboard = () => {
   }
 
   return {
+    getBoard,
     getDeployed,
     placeShip,
     whoIsIn,
     receiveAttack,
-    getRecord,
+    getMissedShots,
     isAllSunk
   }
 }
