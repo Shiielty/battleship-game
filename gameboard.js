0fb62ff4d7dc9e8d91bbdb35e8514959b5b6862f
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-03-07 21:47:28 +0700

    delete concole.log and fix test suite

--- gameboard.js
+++ gameboard.js
@@ -21,10 +21,8 @@ const Gameboard = () => {
     const inCoordinate = whoIsIn(coordinate);
     if (inCoordinate !== null) {
       inCoordinate.hit();
-      console.log(`ships receive attack at ${coordinate}`);
     } else {
       missedShots.push(coordinate);
-      console.log(`missed at ${coordinate}`);
     }
   };
 
